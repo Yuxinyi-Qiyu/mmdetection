642c54404147003210ee0467889eb1e3e46e4363
Xinyi <1758734701@qq.com>
2022-03-07 00:30:11 +0800

    Initial commit

--- mmdet/models/backbones/csp_darknet.py
+++ mmdet/models/backbones/csp_darknet.py
@@ -265,69 +265,6 @@ class CSPDarknet(BaseModule):
             layer = getattr(self, layer_name)
             print("i="+str(i)+":"+str(layer.type))
 
-        # base_channel = 32
-        # factor = [1, 2, 4, 8, 16]
-
-        # for i, layer_name in enumerate(self.layers):
-        #     layer = getattr(self, layer_name)
-        #     if layer_name == "stem":
-        #         print("stem:i="+str(i))
-        #         layer.conv.conv.out_channels = int(base_channel * widen_factor)
-        #         layer.conv.bn.num_features = int(base_channel * widen_factor)
-        #         continue
-        #
-        #     arch_setting = self.arch_settings['P5']
-        #     _, _, num_blocks, _, use_spp = arch_setting[i - 1]
-        #     print(num_blocks,use_spp)
-        #     num_blocks = max(round(num_blocks * 0.33), 1) # deepfactor
-        #     use_spp_x = 1 if use_spp else 0
-        #     print(use_spp_x)
-            # # convmodule
-            # in_channel = int(base_channel * widen_factor) * factor[i - 1]
-            # out_channel = int(base_channel * widen_factor) * factor[i]
-            # layer[0].conv.in_channels, layer[0].conv.out_channels = in_channel, out_channel
-            # layer[0].bn.num_features = out_channel
-            # if use_spp:
-            #     # sppbottleneck  ?conv2_channels = mid_channels * (len(kernel_sizes) + 1)
-            #     in_channel = out_channel
-            #     mid_channel = in_channel // 2
-            #     out_channel = out_channel
-            #     layer[1].conv1.conv.in_channels, layer[1].conv1.conv.out_channels = in_channel, mid_channel
-            #     layer[1].conv1.bn.num_features = mid_channel
-            #     layer[1].conv2.conv.in_channels, layer[1].conv2.conv.out_channels = mid_channel * 4, out_channel
-            #     layer[1].conv2.bn.num_features = out_channel
-            # CSPlayer mid_channels = int(out_channels * expand_ratio)
-            # num_blocks = max(round(num_blocks * deepen_factor), 1)
-            # in_channel = out_channel
-            # mid_channel = int(out_channel * 0.5)
-            # layer[1 + use_spp_x].main_conv.conv.in_channels, layer[1 + use_spp_x].main_conv.conv.out_channels = in_channel, mid_channel
-            # layer[1 + use_spp_x].main_conv.bn.num_features = mid_channel
-            # layer[1 + use_spp_x].short_conv.conv.in_channels, layer[1 + use_spp_x].short_conv.conv.out_channels = in_channel, mid_channel
-            # layer[1 + use_spp_x].short_conv.bn.num_features = mid_channel
-            # layer[1 + use_spp_x].final_conv.conv.in_channels, layer[1 + use_spp_x].final_conv.conv.out_channels = mid_channel * 2, out_channel
-            # layer[1 + use_spp_x].final_conv.bn.num_features = out_channel
-           # # DarknetBottleneck
-            # darknetbottleneck = layer[1 + use_spp_x].blocks  # Sequential
-            # for block_num in range(num_blocks):
-            #     hidden_channel = int(mid_channel * 1.0)
-            #     darknetbottleneck[block_num].conv1.conv.in_channels, darknetbottleneck[block_num].conv1.conv.out_channels = mid_channel, hidden_channel
-            #     darknetbottleneck[block_num].conv1.bn.num_features = hidden_channel
-            #     darknetbottleneck[block_num].conv2.conv.in_channels, darknetbottleneck[0].conv2.conv.out_channels = hidden_channel, mid_channel
-            #     darknetbottleneck[block_num].conv2.bn.num_features = mid_channel
-        # 每个stage的in_channel对应basechannel的倍数
-        # for i, layer_name in enumerate(self.layers):
-        #     layer = getattr(self, layer_name)
-        #     if layer_name == "stem":
-        #         layer.conv.conv.out_channels = int(base_channel * widen_factor)
-        #         layer.conv.bn.num_features = int(base_channel * widen_factor)
-        #     else:
-        #         layer[0].conv.in_channels, layer[0].conv.out_channels = int(base_channel * widen_factor) * factor[i-1], int(base_channel * widen_factor) * factor[i]
-        #         layer[0].bn.num_features = int(base_channel * widen_factor) * factor[i]
-
-        # for i, layer_name in enumerate(self.layers):
-        #     layer = getattr(self, layer_name)
-        #     print("i="+str(i)+":"+str(layer))
-
     def _freeze_stages(self):
         if self.frozen_stages >= 0:
             for i in range(self.frozen_stages + 1):
@@ -350,7 +287,6 @@ class CSPDarknet(BaseModule):
             layer = getattr(self, layer_name)
             print(layer)
             print(x.shape)
-
             x = layer(x)
             print(x.shape)
             print("fin!")
